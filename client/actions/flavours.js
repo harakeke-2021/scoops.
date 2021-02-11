f4168ed26cb78712d8d4838a8fb1b6c1ff82a3ea
Anna McCormack <anna.mccormack94@gmail.com>
2021-02-12 11:49:15 +1300

    edit reducers & actions

--- client/actions/flavours.js
+++
@@ -1,9 +0,0 @@
-import { getFlavours } from '../api'
-
-export const FETCH_FLAVOURS = 'FETCH_FLAVOURS'
-
-export function fetchFalvours () {
-  return (dispatch) => {
-    
-  }
-}
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,11 +1,8 @@
 import { combineReducers } from 'redux'
 
-import toppings from './toppings'
-import flavours from './flavours.js'
-import cones from './cones'
+import iceCream from './iceCream'
+
 
 export default combineReducers({
-  toppings,
-  flavours,
-  cones
+  iceCream
 })
