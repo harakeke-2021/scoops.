0525952165a4eb0245d8db2964203cd4594951cb
Multi Ah Foon <35914987+multiahfoon@users.noreply.github.com>
2021-02-12 12:19:08 +1300

    add image links to database

--- server/db/seeds/products.js
+++ server/db/seeds/products.js
@@ -6,23 +6,24 @@ exports.seed = (knex) => {
           id: 1,
           name: 'Basic Cone',
           category: 'cones',
-          description: '',
+          description: 'Standard ice cream cone',
           price: 1,
-          image: ''
+          image: '/images/cones.jpg'
         },
         {
           id: 2,
           name: 'Waffle Cone',
           category: 'cones',
-          description: '',
+          description: 'Its all about the upgrades',
           price: 2,
-          image: ''
+          image: '/images/waffle-cone.jpg'
         },
         {
+          // need to find an image for cup
           id: 3,
           name: 'Cup',
           category: 'cones',
-          description: '',
+          description: 'No limits here, put everything in a mini bowl',
           price: 1.50,
           image: ''
         },
@@ -31,16 +32,16 @@ exports.seed = (knex) => {
           name: 'Chocolate',
           category: 'flavours',
           description: 'A New Zealands classic. A rich flavoured ice cream.',
-          price: 3,
-          image: ''
+          price: 6,
+          image: '/images/chocolate.png'
         },
         {
           id: 5,
           name: 'Choc Salted Caramel',
           category: 'flavours',
           description: 'Caramel flavoured ice cream with a chocolate saulted ripple.',
-          price: 3,
-          image: ''
+          price: 7,
+          image: 'chocolate-salted-caramel.png'
         },
         {
           id: 6,
@@ -48,39 +49,39 @@ exports.seed = (knex) => {
           category: 'flavours',
           description: 'The winner of the Best of Nuts category.',
           price: 3,
-          image: ''
+          image: '/images/hazelnut.png'
         },
         {
           id: 7,
           name: 'Lemon Lime',
           category: 'flavours',
           description: 'Lemon sorbet flavoured with a lime ripple.',
-          price: 3,
-          image: ''
+          price: 5,
+          image: '/images/lemon-lime.png'
         },
         {
           id: 8,
           name: 'Mango',
           category: 'flavours',
           description: 'Mango flavoured sorbet with a mango ripple.',
-          price: 3,
-          image: ''
+          price: 7,
+          image: 'mango.png'
         },
         {
           id: 9,
           name: 'Mint Choc',
           category: 'flavours',
           description: 'Mint flavoured frozen ice cream with a mango ripple.',
-          price: 3,
-          image: ''
+          price: 6,
+          image: '/images/mint-chocolate.png'
         },
         {
           id: 10,
           name: 'Raspberry',
           category: 'flavours',
           description: 'Raspberry sorbet flavoured frozen ice cream.',
-          price: 3,
-          image: ''
+          price: 6,
+          image: '/images/raspberry.png'
         },
         {
           id: 11,
@@ -88,29 +89,29 @@ exports.seed = (knex) => {
           category: 'flavours',
           description: 'Rum flavoured frozen ice cream with dipped raisins.',
           price: 3,
-          image: ''
+          image: '/images/rum-raisin.png'
         },
         {
           id: 12,
           name: 'Strawberry',
           category: 'flavours',
           description: 'A summer fav frozen ice cream made with fresh strawberry.',
-          price: 3,
-          image: ''
+          price: 4,
+          image: '/images/strawberry.png'
         },
         {
           id: 13,
           name: 'Vanilla',
           category: 'flavours',
           description: 'A New Zealand classic flavour we all know and love, the taste of summer and birthdays.',
-          price: 3,
-          image: ''
+          price: 4,
+          image: '/images/vanilla.png'
         },
         {
           id: 14,
           name: 'Cherry',
           category: 'toppings',
-          description: '',
+          description: "Only pick this if it's for insta or something",
           price: 0.5,
           image: '/images/cherry.jpg'
         },
@@ -118,7 +119,7 @@ exports.seed = (knex) => {
           id: 15,
           name: 'Chopped Walnuts',
           category: 'toppings',
-          description: '',
+          description: "Let people know you're on a diet",
           price: 0.5,
           image: '/images/chopped-walnuts.jpg'
         },
@@ -126,7 +127,7 @@ exports.seed = (knex) => {
           id: 16,
           name: 'Sprinkles',
           category: 'toppings',
-          description: '',
+          description: 'Sprinkles, what else can I say',
           price: 0.25,
           image: '/images/sprinkles.jpg'
         },
@@ -134,7 +135,7 @@ exports.seed = (knex) => {
           id: 17,
           name: 'Chocolate Chips',
           category: 'toppings',
-          description: '',
+          description: "Can't go wrong with chocolate chip.",
           price: 0.5,
           image: '/images/choc-chips.jpg'
         }
