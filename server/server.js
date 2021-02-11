dcdfe6b81360305a1adaa879cb073f678f770012
sirchras <cjlynch@hotmail.co.nz>
2021-02-12 12:33:33 +1300

    add GET /api/v1/orders/ route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 
 const iceCream = require('./routes/ice-cream')
+const orders = require('./routes/orders')
 
 const server = express()
 
@@ -9,5 +10,6 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
 server.use('/api/v1/ice-cream', iceCream)
+server.use('/api/v1/orders', orders)
 
 module.exports = server
