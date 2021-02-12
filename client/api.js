130a6023c65912b88a64a7e3bbb24854470c4cf5
Anna McCormack <anna.mccormack94@gmail.com>
2021-02-12 13:34:33 +1300

    redo  api fns

--- client/api.js
+++ client/api.js
@@ -2,16 +2,15 @@ import request from 'superagent'
 
 export function getAll () {
   return request.get('/api/v1/ice-cream')
-  .then(res => {
-    const all = res.body
-    return {
-      flavours: getFlavours(all),
-      toppings: getToppings(all),
-      cones: getCones(all)
-    }
-  })
+    .then(res => {
+      const all = res.body
+      return {
+        flavours: getFlavours(all),
+        toppings: getToppings(all),
+        cones: getCones(all)
+      }
+    })
 }
-
 
 function getFlavours (all) {
   return all.filter((product) => {
@@ -19,14 +18,13 @@ function getFlavours (all) {
   })
 }
 
-function getTOppings (all) {
+function getToppings (all) {
   return all.filter((product) => {
     return product.category === 'toppings'
   })
 }
 
-
-function getFlavours(all) {
+function getCones (all) {
   return all.filter((product) => {
     return product.category === 'cones'
   })
