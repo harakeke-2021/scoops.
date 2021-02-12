0b225ed4c4a78024c8bc323ec8ecca183cfe097b
Anna McCormack <anna.mccormack94@gmail.com>
2021-02-12 14:56:05 +1300

    set up cones and toppings toggle

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link } from 'react-router-dom'
 
@@ -7,11 +7,35 @@ import Cones from './Cone'
 import Toppings from './Toppings'
 
 function Home () {
+  const [showFlavours, setShowFLavours] = useState(false)
+  const [showToppings, setShowToppings] = useState(false)
+  const [showCones, setShowCones] = useState(false)
+
+  const toggleFlavours = () => {
+    setShowFLavours(!showFlavours)
+  }
+
+  const toggleToppings = () => {
+    setShowToppings(!showToppings)
+  }
+
+  const toggleCones = () => {
+    setShowCones(!showCones)
+  }
+
   return (
 
     <div className='home'>
       <div className='categories'>
-        <Flavours />
+        <button onClick={toggleCones}>Cones</button>
+        {showCones && <Cones /> }
+
+        <button onClick={toggleFlavours}>Flavours</button>
+        {showFlavours && <Flavours /> }
+
+        <button onClick={toggleToppings}>Toppings</button>
+        {showToppings && <Toppings /> }
+
         {/* <Cones/>
         <Toppings /> */}
       </div>
